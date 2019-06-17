d6963e8e2fedecfcbd64b01ba83c6f07632f09f9
msiclaver <claver.developper@gmail.com>
2019-06-17 20:08:57 +0300

    Add files via upload

--- src/components/Datable.vue
+++ src/components/Datable.vue
@@ -1,45 +1,81 @@
+<script>
+/* 
+ <datable 
+          :data-headers="thead"
+          :data-entries="datas" 
+          :is-paginable="true"
+          :pagin-color="paginatorColor"
+        />
+         data(){
+     return {
+       thead: ['Id', 'Name', 'Email', 'Address','<input type="checkbox" />'],
+       datas: [],
+       paginatorColor: '#00C384'
+     };
+   },
+   mounted(){
+    for(var i=1; i<=1000;i++){
+         this.datas.push([
+             i,'John Doe','john.doe@lukkon.com', 'Chicago, brains 0' + i
+         ]);
+       }
+   },
+*/
+</script>
+
 <template>
     <div class="datable">
-            <table class="table">
-            <thead >
-            <tr>
-              <th v-for="h in dataHeaders" :key="h" v-html="h"></th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="item in mergeDatas" :key="item">
-                <td>{{ item.id }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.email}}</td>
-                <td>{{ item.address }}</td>
-                <td></td>
-            </tr>
-            </tbody>
-            </table>
+      <div class="toolbar">
+        <div class="filter">
+          <select :style="styles.filter" v-model="linesPerPage">
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+        </div>
+        <div class="search">
+          <input type="text" :style="styles.search" placeholder="search">
+        </div>
+      </div>
+      <table class="table">
+        <thead >
+          <tr>
+            <th v-for="h in dataHeaders" :key="h" v-html="h" :style="styles.header"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item,i) in mergeDatas" :key="i">
+              <td v-for="(td,t) in item" :key="t">{{ td }}</td>
+              <td></td>
+          </tr>
+        </tbody>
+      </table>
             <!-- pagination -->
-            <div class="paginator" :style="paginStyle"  v-if="isPaginable">
-        <button class="btn" :style="btnStyle" @click.prevent="prev">&lt;&lt;</button>
-        <div v-if="currentPage>3">
-          <button class="btn" :style="[btnStyle, currentPage==1 ? btnSelStyle : '' ]"
-          @click.prevent="currentPage=1" 
-              >{{ '1' }}</button>
-          <span>...</span>
-        </div>
-        <button class="btn" v-for="p in pages.slice(currentPage-1,currentPage+2)" :key="p"
-        @click.prevent="currentPage=p" 
-        :style="[btnStyle, p==currentPage ? btnSelStyle : '' ]">
-        {{ p }}
-        </button>
-        <div v-if="(currentPage+5)<pages.length">
-          <span>...</span>
-          <button class="btn" :style="[btnStyle, currentPage==pages.length ? btnSelStyle : '' ]" 
-          @click.prevent="currentPage=pages.length"
-            >{{ pages.length }}</button>
-        </div>
-        <button class="btn" :style="btnStyle" @click.prevent="next">&gt;&gt;</button>
+    <div class="paginator" :style="styles.paginator.default"  v-if="isPaginable">
+      <button class="btn" :style="styles.paginator.button" @click.prevent="prev">&lt;&lt;</button>
+      <div v-if="currentPage>3">
+        <button class="btn" :style="[styles.paginator.button, currentPage==1 ? styles.paginator.buttonSelected : '' ]"
+        @click.prevent="currentPage=1" 
+            >{{ '1' }}</button>
+        <span>...</span>
+      </div>
+      <button class="btn" v-for="p in pages.slice(currentPage-1,currentPage+2)" :key="p"
+      @click.prevent="currentPage=p" 
+      :style="[styles.paginator.button, p==currentPage ? styles.paginator.buttonSelected : '' ]">
+      {{ p }}
+      </button>
+      <div v-if="(currentPage+5)<pages.length">
+        <span>...</span>
+        <button class="btn" :style="[styles.paginator.button, currentPage==pages.length ? styles.paginator.buttonSelected : '' ]" 
+        @click.prevent="currentPage=pages.length"
+          >{{ pages.length }}</button>
+      </div>
+      <button class="btn" :style="styles.paginator.button" @click.prevent="next" >&gt;&gt;</button>
     </div>
 
-         </div>
+  </div>
 </template>
 <script>
 export default {
@@ -48,42 +84,59 @@ export default {
         dataEntries: {type: Array, required: true},
         perPage: {type: Number, default: 10},
         isPaginable: {type: Boolean, default: true},
-        color: {type: String, default: '#28B1E4'}
+        accentColor: {type: String, default: '#aaa'},
+        headerTextColor: {type: String, default: '#555'},
+        headerColor: {type: String, default: 'transparent'},
     },
     data(){
      return {
        currentPage: 1,
+       linesPerPage: this.perPage
      };
     },
    computed: {
-  paginStyle () {
-    return {
-        display: 'flex',
-        justifyContent: 'flex-end',
-        color: this.color
-    }
-  },
-  btnStyle(){
-    return {
-     border: '1px solid '+ this.color,
-      outline: 'none',
-      padding: '5px 10px',
-      borderRadius: '3px',
-      margin: '5px',
-      color: this.color,
-      background: '#fff',
-    }
-  },
-  btnSelStyle(){
-    return {
-      color: '#fff',
-      background: this.color
-    }
-  },
+     styles() {
+       return {
+         header: {
+           background: this.headerColor,
+           color: this.headerTextColor,
+         },
+         paginator: {
+            default: {
+              display: 'flex',
+              justifyContent: 'flex-end',
+              color: this.accentColor
+            },
+            button:{
+              cursor: 'pointer',
+              border: '1px solid '+ this.accentColor,
+              outline: 'none',
+              padding: '5px 10px',
+              borderRadius: '3px',
+              margin: '5px',
+              color: this.accentColor,
+              background: '#fff', 
+            },
+            buttonSelected: {
+              color: '#fff',
+              background: this.accentColor
+            },
+
+            
+         },
+         filter: {
+           border: '1px solid '+ this.accentColor,
+           color:  this.accentColor
+         },
+         search: {
+           border: '1px solid '+ this.accentColor,
+           color:  this.accentColor
+         }
+       }
+     },
      pages(){
        var page = []
-        for(let p=1; p<= Math.ceil(this.dataEntries.length/this.perPage); p++){
-          console.log(p)
+        for(let p=1; p<= Math.ceil(this.dataEntries.length/this.linesPerPage); p++){
           page.push(p)
         }
         return page;
@@ -104,12 +157,63 @@ export default {
        }
      },
      paginate(datas){
-       console.log(this.currentPage+'/'+this.perPage)
-       console.log(datas.length)
-       let from = (this.currentPage*this.perPage) - this.perPage
-       let to = this.currentPage * this.perPage
+       let from = (this.currentPage*this.linesPerPage) - this.linesPerPage
+       let to = this.currentPage * this.linesPerPage
        return datas.slice(from, to)
      }
    }
 }
+
 </script>
+ <style>
+ .datable .table{
+  background: #fff;
+  width: 100%;
+  margin: 10px auto;
+  border-collapse: collapse;
+}
+.datable .table th, td{
+  text-align: left;
+}
+
+.datable .table th{
+  padding: 10px 10px;
+  border-bottom: 1px solid #ccc;
+}
+.datable .table td{
+  padding: 5px 10px;
+  border-bottom: 1px solid #f5f5f5;
+}
+.datable .btn-pagin-group{
+  display: flex;
+  justify-content: flex-end;
+}
+.datable .btn-pagin{
+  background: #fff;
+  border: 1px solid #ddd;
+  color: #555;
+  outline: none;
+  padding: 5px 10px;
+  border-radius: 3px;
+  margin: 5px;
+}
+.datable .btn-pagin.selected{
+  color: #444;
+  background: #eee;
+}
+.datable .toolbar{
+  display: flex;
+  justify-content: space-between;
+}
+.datable .toolbar .filter select{
+  outline: none;
+  padding: 5px 10px;
+  background: transparent;
+  border-radius: 2px;
+}.datable .toolbar .search input{
+  outline: none;
+  padding: 5px 10px;
+  background: transparent;
+  border-radius: 2px;
+}
+    </style>
